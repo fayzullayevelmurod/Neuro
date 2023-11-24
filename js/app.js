5e9ce0f66bfdff17656f13546f375f5415dd76ba
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-11-24 21:25:44 +0500

    the end

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mainSwipper", {
+    loop: true,
     slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".btn_right",
     },
 });
